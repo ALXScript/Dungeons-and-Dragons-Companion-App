d25e4a4d35f4ebe709de2c93d3431d6d42df710c
Michael Osborne <unfazed90@mail.fresnostate.edu>
2018-10-14 08:51:47 -0700

    testing inventory feature branch

--- app/src/main/java/com/example/victor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/victor/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //testing feature inventory feature branch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
