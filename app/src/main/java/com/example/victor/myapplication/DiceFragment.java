a36156c3b5e0d5739ee75cd6ad379904311ab34f
victor <Victorplopez@yahoo.com>
2018-10-14 21:43:58 -0700

    Dice Functionality

--- app/src/main/java/com/example/victor/myapplication/DiceFragment.java
+++ app/src/main/java/com/example/victor/myapplication/DiceFragment.java
@@ -7,11 +7,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class DiceFragment extends Fragment {
+    private ImageView imageViewDice;
+    private Button buttonDice;
+    private Random rng = new Random();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_diceRoller, container, false);
+        return inflater.inflate(R.layout.fragment_diceroller, container, false);
     }
+
+    protected void logic(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_diceroller);
+
+        buttonDice = buttonDice.findViewById(R.id.d6);
+        buttonDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice(6);
+            }
+        });
+    }
+
+
+    private void rollDice(int diceNum){
+        int randomNumber = rng.nextInt(diceNum)+1;
+
+        switch (randomNumber) {
+            case 1:
+                imageViewDice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageViewDice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageViewDice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageViewDice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageViewDice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageViewDice.setImageResource(R.drawable.dice6);
+                break;
+        }
+    }
+
 }
