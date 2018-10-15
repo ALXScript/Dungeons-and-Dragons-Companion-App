fc6b086ed5ae12c17c984e2f0487f9799c6b931b
victor <Victorplopez@yahoo.com>
2018-10-14 19:28:16 -0700

    Changed name of fragment

--- app/src/main/java/com/example/victor/myapplication/DiceFragment.java
+++ app/src/main/java/com/example/victor/myapplication/DiceFragment.java
@@ -12,6 +12,6 @@ public class DiceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        return inflater.inflate(R.layout.fragment_diceRoller, container, false);
     }
 }
