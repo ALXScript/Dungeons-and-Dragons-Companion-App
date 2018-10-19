ecef9580438e4cede5f00bf1356cfafe123aaed2
Michael Osborne <mikeosborne1990@gmail.com>
2018-10-18 21:37:09 -0700

    Added functionality to Creat Char button on HomeFragment.

--- app/src/main/java/com/example/victor/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/victor/myapplication/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.victor.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+
+    private Button charCreateBtn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        super.onCreate(savedInstanceState);
+
+        charCreateBtn = view.findViewById(R.id.creatCharBtn);
+
+        charCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), createChar.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
