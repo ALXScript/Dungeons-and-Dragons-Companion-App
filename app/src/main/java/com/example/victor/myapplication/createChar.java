b9e6b3cb0bcfc6cdfc6de088a934e89cf2ecce69
Alex <alexiscontrerase9@gmail.com>
2018-10-29 15:05:40 -0700

    added dwarf json object as a sample

--- app/src/main/java/com/example/victor/myapplication/createChar.java
+++ app/src/main/java/com/example/victor/myapplication/createChar.java
@@ -2,35 +2,96 @@ package com.example.victor.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import com.example.victor.myapplication.DClass;
+import com.example.victor.myapplication.Race;
 
-import com.squareup.otto.Subscribe;
 
+//extends AppCompatActivity needed to be able to go from one activity to another
 public class createChar extends AppCompatActivity
 {
+    //A Tag to reference the class (not that important, may not be needed)
     private static final String TAG = "createChar";
-    TextView textView;
+
+    //******STARTING DEFINING VARIABLES********
+    //Imported Classes variables classes
+
+
+    //myRace.setRace();
+
+    //Stat Variables
+    private String myName;
+    private int myCurrentHP;
+    private int myMaxHP;
+    private int myArmorClass;
+    private String myHitDice;
+    private int myInitiative;
+    private int mySpeed;
+    private int myDSSuccesses;
+    private int myDSFailures;
+
+    //Ability Scores (SDCIWC) Stats
+    /*Array Legend
+    0 - Strength
+    1 - Dexterity
+    2 - Constitution
+    3 - intelligence
+    4 - Wisdom
+    5 - Charisma*/
+    private int myAbilityScores[] = new int[6];
+
+    //Other Stat Variables
+    private boolean statInspiration;
+    private int statProficiencyBonus;
+
+    //Saving Throw Variables
+    /* Array Legend
+    0 - Strength
+    1 - Dexterity
+    2 - Constitution
+    3 - intelligence
+    4 - Wisdom
+    5 - Charisma*/
+    private boolean savingThrowActive[] = new boolean[6];
+    private int savingThrow[] = new int[6];
+
+    //Skills Variable
+    /* Array Legend
+    0 - Acrobatics (Dex)
+    1 - Animal Handling (Wis)
+    2 - Arcana (int)
+    3 - Athletics (Str)
+    4 - Deception (Cha)
+    5 - History (int)
+    6 - Insight (Wis)
+    7 - intimidation (Cha)
+    8 - Investigation (int)
+    9 - Medicine (Wis)
+    10 - Nature (int)
+    11 - Perception (Wis)
+    12 - Performance (Cha)
+    13 - Persuasion (Cha)
+    14 - Religion (int)
+    15 - Sleight of Hand (Dex)
+    16 - Stealth (Dex)
+    17 - Survival (Wis)
+    */
+    private boolean skillsActive[] = new boolean[18];
+    private int skills[] = new int[18];
+
+    //Char Creation Variables
+    private int mySpellBookTable;
+    private int myInventoryTable;
+
+
+
+    //Default Constructor
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        //references the XML in Layout Folder
         setContentView(R.layout.createchar);
-        textView=findViewById(R.id.textView);
-        BusProvider.getInstance().register(this);
     }
 
-    /*
-
-    Subscription is the complement to event publishing—it lets you receive notification
-     that an event has occurred. To subscribe to an event, annotate a method with
-     @Subscribe. The method should take only a single parameter,
-     the type of which will be the event you wish to subscribe to.
-     You DONT need to call the function, the Otto Bus API will do so
-     In order to receive an event you need to register with the Bus
-     */
-    @Subscribe
-    public void getCharacter(String s)
-    {
-        if (textView!=null) textView.setText(s);
-    }
 }
