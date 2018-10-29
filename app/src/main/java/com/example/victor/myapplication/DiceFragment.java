c406bfc0b6971c8c69ecd2a719f276328fbfcfbf
victor <Victorplopez@yahoo.com>
2018-10-28 18:42:52 -0700

    Dice Roller working and displaying individual Dice rolls. D4 + D6 functionality only

--- app/src/main/java/com/example/victor/myapplication/DiceFragment.java
+++ app/src/main/java/com/example/victor/myapplication/DiceFragment.java
@@ -4,19 +4,51 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 
 import java.util.Random;
 
-public class DiceFragment extends Fragment {
-    private ImageView imageViewDice;
-    private Button buttonDice;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+public class DiceFragment extends Fragment implements View.OnClickListener{
+    private ImageView d6;
+    private Button rollButton, d4;
+    private TextView diceResultTextView , diceQuant;
+
     private Random rng = new Random();
+    private int[] diceCount = {0,0,0,0,0,0,0};//Number of dice being rolled
+    private int[] diceResults = {0,0,0,0,0,0,0};//Size of dice. d4, d6, d8, etc.
+
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.d4: //diceCount[0]
+                diceCount[0]++;
+                if (diceCount[1] > 0)
+                    diceQuant.setText(String.valueOf(diceCount[0])+"d4 + "+String.valueOf(diceCount[1])+"d6");
+                else
+                    diceQuant.setText(String.valueOf(diceCount[0])+"d4");
+                break;
+            case R.id.d6:
+                diceCount[1]++;
+                diceQuant.setText(String.valueOf(diceCount[1])+"d6");
+                //rollDice(6, count);
+                break;
+            case R.id.roll:
+                rollDice();
+                diceCount[0] = diceCount[1] = 0; //Resetting all counts
+                break;
+        }
+    }
+
 
     @Nullable
     @Override
@@ -24,41 +56,100 @@ public class DiceFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_diceroller, container, false);
         super.onCreate(savedInstanceState);
 
-        imageViewDice = view.findViewById(R.id.image_view_dice);
-        buttonDice = view.findViewById(R.id.d6);
-        buttonDice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rollDice(6);
-            }
+        //Onclick listeners
+        d4 = view.findViewById(R.id.d4);
+        d4.setOnClickListener(this);
 
-            public void rollDice(int diceNum) {
-                int randomNumber = rng.nextInt(diceNum) + 1;
+        d6 = view.findViewById(R.id.d6);
+        d6.setOnClickListener(this);
 
-                switch (randomNumber) {
-                    case 1:
-                        imageViewDice.setImageResource(R.drawable.dice1);
-                        break;
-                    case 2:
-                        imageViewDice.setImageResource(R.drawable.dice2);
-                        break;
-                    case 3:
-                        imageViewDice.setImageResource(R.drawable.dice3);
-                        break;
-                    case 4:
-                        imageViewDice.setImageResource(R.drawable.dice4);
-                        break;
-                    case 5:
-                        imageViewDice.setImageResource(R.drawable.dice5);
-                        break;
-                    case 6:
-                        imageViewDice.setImageResource(R.drawable.dice6);
-                        break;
-                }
-            }
-        });
+        rollButton = view.findViewById(R.id.roll);
+        rollButton.setOnClickListener(this);
+
+
+        diceQuant = view.findViewById(R.id.diceQuant);
+        diceResultTextView = view.findViewById(R.id.diceResultTextView);
+
+
+
 
         return view;
     }
 
+
+    private void rollDice(){
+        int[] diceSize = {4,6,8,10,12,20,100};
+        int sum=0;
+        String individualDiceRoll = "Hi", newRoll;
+        boolean firstRoll = TRUE;
+
+        for (int j= 0; j<6; j++) {
+            for (int i = 0; i < diceCount[j]; i++) {
+                diceResults[i] = rng.nextInt(diceSize[j]) + 1;
+                if (firstRoll == TRUE) {
+                    individualDiceRoll = Integer.toString(diceResults[i]);
+                    firstRoll = FALSE;
+                }
+                else {
+                    newRoll = " + " + Integer.toString(diceResults[i]);
+                    individualDiceRoll += newRoll;
+                }
+                sum += diceResults[i];
+            }
+        }
+        diceResultTextView.setText(individualDiceRoll+" = "+String.valueOf(sum));
+    }
 }
+
+/*
+        imageViewDice = view.findViewById(R.id.image_view_dice);
+        d6 = view.findViewById(R.id.d6);
+        diceResult = view.findViewById(R.id.diceResult);
+        d6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                diceResult.setText(String.valueOf(count)+"d6 = ");
+                rollDice(6, count);
+            }
+
+            private void rollDice(int diceNum, int count) {
+                //int randomNumber = 0;
+                int sum = 0;
+                //diceResult.setText(String.valueOf(randomNumber));
+
+
+                for (int i =0; i<count+1; i++){
+                    sum += rng.nextInt(diceNum) + 1;
+                }
+                diceResult.setText(String.valueOf(count)+"d6 = "+String.valueOf(sum));
+
+//                switch (randomNumber) {
+//                    case 1:
+//                        imageViewDice.setImageResource(R.drawable.dice1);
+//                        break;
+//                    case 2:
+//                        imageViewDice.setImageResource(R.drawable.dice2);
+//                        break;
+//                    case 3:
+//                        imageViewDice.setImageResource(R.drawable.dice3);
+//                        break;
+//                    case 4:
+//                        imageViewDice.setImageResource(R.drawable.dice4);
+//                        break;
+//                    case 5:
+//                        imageViewDice.setImageResource(R.drawable.dice5);
+//                        break;
+//                    case 6:
+//                        imageViewDice.setImageResource(R.drawable.dice6);
+//                        break;
+//                }
+            }
+
+
+
+
+
+        });
+
+ */
