feca291edcc262d858d1b74deb6a6fd34d749de2
Michael Osborne <mikeosborne1990@gmail.com>
2018-10-31 09:17:13 -0700

    Added DatabaseHelper, DatabaseAccess, and ListItembook classes.

---
+++ app/src/main/java/com/example/victor/myapplication/DatabaseAccess.java
@@ -0,0 +1,52 @@
+package com.example.victor.myapplication;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DatabaseAccess {
+
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static DatabaseAccess instance;
+
+
+    private DatabaseAccess(Context context) {
+        this.openHelper = new DatabaseHelper(context);
+    }
+
+    public static DatabaseAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    public List<String> getItemNames() {
+        List<String> list = new ArrayList<>();
+        Cursor cursor = database.rawQuery("SELECT * FROM items", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getString(1));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+
+}
--- app/src/main/java/com/example/victor/myapplication/InventoryFragment.java
+++ app/src/main/java/com/example/victor/myapplication/InventoryFragment.java
@@ -1,5 +1,6 @@
 package com.example.victor.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class InventoryFragment extends Fragment {
+
+    private Button itemBookBttn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inventory, container, false);
+        View view = inflater.inflate(R.layout.fragment_inventory, container, false);
+        super.onCreate(savedInstanceState);
+
+        itemBookBttn = view.findViewById(R.id.itemBookBtn);
+
+        itemBookBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ListItembook.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
