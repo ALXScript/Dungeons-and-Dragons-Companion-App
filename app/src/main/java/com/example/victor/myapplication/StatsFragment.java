a96f115e5986f26b08708278d568876d7cfb6b8b
xander <xwinans@mail.fresnostate.edu>
2018-11-03 14:03:08 -0700

    Added RecylerView for AbilityScores

--- app/src/main/java/com/example/victor/myapplication/StatsFragment.java
+++ app/src/main/java/com/example/victor/myapplication/StatsFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import java.util.Objects;
@@ -15,23 +18,20 @@ import java.util.Objects;
 
 public class StatsFragment extends Fragment {
 
-    ProgressBar healthBar;
-    Button increaseHealthButton;
+    RecyclerView abilityScoreRecycler;
+    AbilityScoreAdapter adapter;
+    String [] abilityScoreNames;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stats, container, false);
+        abilityScoreRecycler = view.findViewById(R.id.recyclerViewAbilityScores);
+        abilityScoreRecycler.setHasFixedSize(true);
+        abilityScoreRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        abilityScoreNames = getResources().getStringArray(R.array.AbilityScores);
 
-        healthBar = view.findViewById(R.id.healthProgressBar);
-        increaseHealthButton = view.findViewById(R.id.increaseHealthButton);
-
-        increaseHealthButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                healthBar.incrementProgressBy(1);
-            }
-        });
+        adapter = new AbilityScoreAdapter(getContext(),abilityScoreNames);
+        abilityScoreRecycler.setAdapter(adapter);
         return view;
     }
 }
