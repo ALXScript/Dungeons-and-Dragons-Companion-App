2d9cbfd6d3afcd522c429828ef53af283e3a752e
xander <xwinans@mail.fresnostate.edu>
2018-11-03 09:54:05 -0700

    Added strings for ability scores

--- app/src/main/java/com/example/victor/myapplication/StatsFragment.java
+++ app/src/main/java/com/example/victor/myapplication/StatsFragment.java
@@ -7,11 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import java.util.Objects;
+
 
 public class StatsFragment extends Fragment {
+
+    ProgressBar healthBar;
+    Button increaseHealthButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        healthBar = view.findViewById(R.id.healthProgressBar);
+        increaseHealthButton = view.findViewById(R.id.increaseHealthButton);
+
+        increaseHealthButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                healthBar.incrementProgressBy(1);
+            }
+        });
+        return view;
     }
 }
