f9ce8ff3a0743442d2efd2073177213e552912b4
Alex <alexiscontrerase9@gmail.com>
2018-11-06 16:25:20 -0800

    Spinner now works, successfully parsing JSON through the selected spinner item

---
+++ app/src/main/java/com/example/victor/myapplication/charClass.java
@@ -0,0 +1,15 @@
+package com.example.victor.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class charClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createcharclass);
+
+    }
+
+}
