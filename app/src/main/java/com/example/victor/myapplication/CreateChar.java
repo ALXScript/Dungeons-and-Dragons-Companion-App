a7319671e34f6c23e4574d05ea1179d07c41edeb
Michael Osborne <mikeosborne1990@gmail.com>
2018-11-13 17:36:44 -0800

    Synching Alexs Branch to develop.

--- app/src/main/java/com/example/victor/myapplication/CreateChar.java
+++ app/src/main/java/com/example/victor/myapplication/CreateChar.java
@@ -1,5 +1,7 @@
 package com.example.victor.myapplication;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -8,29 +10,26 @@ import com.squareup.otto.Subscribe;
 
 public class CreateChar extends AppCompatActivity
 {
-    private static final String TAG = "createChar";
-    TextView textView;
+    //private static final String TAG = "createChar";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.createchar);
-        textView=findViewById(R.id.textView);
-        BusProvider.getInstance().register(this);
+        setContentView(R.layout.activity_createcharacter);
+
+        Fragment frag = new fragment_createchar_race();
+        FragmentManager fragManager = getFragmentManager();
+        fragManager.beginTransaction().replace(R.id.fragment_container, new fragment_createchar_race()).commit();
+
     }
 
     /*
 
-    Subscription is the complement to event publishing—it lets you receive notification
-     that an event has occurred. To subscribe to an event, annotate a method with
-     @Subscribe. The method should take only a single parameter,
-     the type of which will be the event you wish to subscribe to.
-     You DONT need to call the function, the Otto Bus API will do so
-     In order to receive an event you need to register with the Bus
-     */
     @Subscribe
     public void getCharacter(String s)
     {
         if (textView!=null) textView.setText(s);
-    }
+    }*/
 }
+
--- app/src/main/java/com/example/victor/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/victor/myapplication/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.victor.myapplication;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -13,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
