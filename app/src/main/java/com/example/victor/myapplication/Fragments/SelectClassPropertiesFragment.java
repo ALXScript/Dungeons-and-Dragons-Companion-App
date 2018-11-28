7ce78016535e9af705b9ac9be0352b1a644dd3b9
Alex <alexiscontrerase9@gmail.com>
2018-11-27 18:44:22 -0800

    added Select Class Properties Fragment XML and Class to work with for Victor

---
+++ app/src/main/java/com/example/victor/myapplication/Fragments/SelectClassPropertiesFragment.java
@@ -0,0 +1,23 @@
+package com.example.victor.myapplication.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.victor.myapplication.R;
+
+public class SelectClassPropertiesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_select_class_properties, container, false);
+        super.onCreate(savedInstanceState);
+
+        return view;
+    }
+}
